f27b6fb6bfd899311b9733d3077624d9d0f0728a
Teagan glenn <that@teagantotally.rocks>
2023-08-12 03:10:49 -0600

    magical water dish

---
+++ target/5.4.1/argon/obj/src/newlib_stubs.o.d
@@ -0,0 +1,105 @@
+/Users/tglenn/src/MagicalWaterDish/target/5.4.1/argon//obj/./src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ ../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/types.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_types.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_types.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/endian.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_endian.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/select.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/timespec.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/sched.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/types.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/stdlib.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/cstdlib \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/pstl/pstl_config.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdlib.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/reent.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/lock.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/alloca.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_locale.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/string.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/strings.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/string.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/config.h \
+ ../../../services/inc/preprocessor.h ../../../hal/shared/platforms.h
+../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/types.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_newlib_version.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/config.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/ieeefp.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/features.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/cdefs.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_default_types.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stddef.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_types.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_types.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_stdint.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/endian.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/_endian.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/select.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_sigset.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timeval.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/timespec.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_timespec.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/sched.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/types.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/stdlib.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/cstdlib:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/pstl/pstl_config.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdlib.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/_ansi.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/reent.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/lock.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/assert.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/machine/stdlib.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/alloca.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_locale.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/c++/10.2.1/bits/std_abs.h:
+../../../services/inc/debug.h:
+../../../services/inc/service_debug.h:
+../../../services/inc/logging.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/string.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/strings.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/string.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/stdint.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/arm-none-eabi/include/sys/_intsup.h:
+/Users/tglenn/.particle/toolchains/gcc-arm/10.2.1/lib/gcc/arm-none-eabi/10.2.1/include/stdarg.h:
+../../../services/inc/panic.h:
+../../../services/inc/config.h:
+../../../services/inc/preprocessor.h:
+../../../hal/shared/platforms.h:
